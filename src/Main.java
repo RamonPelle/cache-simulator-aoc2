bac414688e8a904eabd934844a9d64b0c8dbd5f1
Ramon <ramon.pelle15@gmail.com>
2023-08-12 20:59:57 -0300

    update: readFile le os binarios e armazena no arraylist como string

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ import java.util.List;
 import core.Cache;
 import utils.Reader;
 
+import utils.Reader;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,14 +19,10 @@ public class Main {
 //        List<String> addr = r.getAddresses();
 //        Cache cache = Cache.build(args[0], args[1], args[2], args[3], args[4]);
 //        System.out.println(cache);
-        byte[] buffer = new byte[4];
-        InputStream is = new FileInputStream("src/resources/addresses/bin_100.bin");
-        int bytes = is.read(buffer);
-        System.out.println(Integer.toBinaryString(buffer[0]));
-        System.out.println(Integer.toBinaryString(buffer[1]));
-        System.out.println(Integer.toBinaryString(buffer[2]));
-        System.out.println(Integer.toBinaryString(buffer[3]));
 //        int intValue = ByteBuffer.wrap(buffer).getInt();
 //        System.out.println("Valor lido: " + intValue);
+
+        Reader r = new Reader();
+        r.readFile("C:\\workspaces\\cache-simulator-aoc2\\src\\resources\\addresses\\bin_100.bin");
     }
 }
--- src/utils/Reader.java
+++ src/utils/Reader.java
@@ -1,8 +1,8 @@
 package utils;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -20,38 +20,47 @@ public class Reader {
         this.addresses = addresses;
     }
 
-    public void readFile(String path) throws FileNotFoundException {
-        File file = new File(path);
-        Scanner sc = new Scanner(file);
-        while (sc.hasNext()) {
-            this.addresses.add(sc.nextLine());
-        }
-        convertToBinary();
-    }
-
-    public void convertToBinary() {
-        List<String> binaryAddresses = new ArrayList<>();
-        for (String address : this.addresses) {
-            Integer intAddress = Integer.parseInt(address);
-            binaryAddresses.add(Integer.toBinaryString(intAddress));
-        }
-        this.addresses = binaryAddresses;
-        fulfill32Bits();
-    }
-
-    public void fulfill32Bits() {
-        List<String> addresses32bits = new ArrayList<>();
-        for (String address : this.addresses) {
-            StringBuilder builder = new StringBuilder();
-            int bitsToFill = 32 - address.length();
-            for (int i = 0; i < bitsToFill; i++) {
-                builder.append("0");
+    public void readFile(String path) throws IOException {
+        byte[] buffer = new byte[4];
+        InputStream is = new FileInputStream(path);
+        int bytes;
+        while ((bytes = is.read(buffer)) == 4) {
+            StringBuilder binaryValue = new StringBuilder();
+            for (byte b : buffer) {
+                for (int i = 7; i >= 0; i--) {
+                    binaryValue.append((b & (1 << i)) == 0 ? "0" : "1");
+                }
             }
-            builder.append(address);
-            addresses32bits.add(builder.toString());
+            addresses.add(binaryValue.toString());
         }
-        this.addresses = addresses32bits;
-        System.out.println(this.addresses);
+        System.out.println(addresses);
+        System.out.println(addresses.size());
+
     }
+
+//    public void convertToBinary() {
+//        List<String> binaryAddresses = new ArrayList<>();
+//        for (String address : this.addresses) {
+//            Integer intAddress = Integer.parseInt(address);
+//            binaryAddresses.add(Integer.toBinaryString(intAddress));
+//        }
+//        this.addresses = binaryAddresses;
+//        fulfill32Bits();
+//    }
+
+//    public void fulfill32Bits() {
+//        List<String> addresses32bits = new ArrayList<>();
+//        for (String address : this.addresses) {
+//            StringBuilder builder = new StringBuilder();
+//            int bitsToFill = 32 - address.length();
+//            for (int i = 0; i < bitsToFill; i++) {
+//                builder.append("0");
+//            }
+//            builder.append(address);
+//            addresses32bits.add(builder.toString());
+//        }
+//        this.addresses = addresses32bits;
+//        System.out.println(this.addresses);
+//    }
 }
 
